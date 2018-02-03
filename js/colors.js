c6755f46ddcb8cd10ff6a4d97106d64222f23efa
nomadlife <joonwoo3816@gmail.com>
2018-02-03 17:46:28 +0900

    study_finished

---
+++ js/colors.js
@@ -0,0 +1,48 @@
+var links = {
+  setColor:function(color){
+  //   var alist = document.querySelectorAll('a');
+  //   var i=0;
+  //   while(i< alist.length){
+  //     alist[i].style.color = color;
+  //     i=i+1;
+  // }
+    $('a').css('color',color);
+  }
+}
+
+var body = {
+  setColor:function (color){
+    // document.querySelector('body').style.color = color;
+    $('body').css('color',color);
+  },
+  setColor_background:function(color){
+    // document.querySelector('body').style.background = color;
+    $('body').css('backgroundColor',color);
+  }
+}
+
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night mode'){
+    body.setColor_background('black')
+    body.setColor('white');
+    self.value = 'day mode';
+    links.setColor('powderblue');
+  } else {
+    body.setColor_background('white')
+    body.setColor('black');
+    self.value = 'night mode';
+    links.setColor('blue');
+  }
+}
+
+function nightHandler(){
+  document.querySelector('body').style.backgroundColor='black';
+  document.querySelector('body').style.color='white';
+}
+
+function dayHandler(){
+  document.querySelector('body').style.backgroundColor='white';
+  document.querySelector('body').style.color='black';
+}
