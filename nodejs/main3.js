8b23b73698b9dffeff680b3df31c75688ff09e2f
nomadlife <joonwoo3816@gmail.com>
2018-08-17 16:54:04 +0900

    node.js 19.2 ongoing

---
+++ nodejs/main3.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    var pathname = url.parse(_url,true).pathname;
+    var title = queryData.id
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            
+            fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+                var template = `
+              <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="index.html">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=css">CSS</a></li>
+              <li><a href="/?id=javascript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+              `;
+              response.writeHead(200);
+              response.end(template);
+              })
+        }
+        
+    } else {
+        response.writeHead(404);
+        response.end('Not Found');
+
+    }
+
+    
+    
+});
+app.listen(3000);
