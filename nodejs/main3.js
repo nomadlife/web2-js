138a576362cb0dfc32105a5c9e3529b3c254264b
nomadlife <joonwoo3816@gmail.com>
2018-08-22 22:12:52 +0900

    nodejs ch37 done

--- nodejs/main3.js
+++ nodejs/main3.js
@@ -12,7 +12,7 @@ function templateHTML(title,list,body,control){
 <meta charset="utf-8">
 </head>
 <body>
-<h1><a href="/">WEB</a></h1>
+<h1><a href="/">WEB5</a></h1>
 ${list}
 ${control}
 ${body}
@@ -58,7 +58,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title,list,
               `<h2>${title}</h2>${description}`,
-            `<a href="/update?id=${title}">update</a>`);
+            `<a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>
+            <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <input type="submit" value="delete">
+            </form>`);
               response.writeHead(200);
               response.end(template);
               })
@@ -71,7 +76,7 @@ var app = http.createServer(function(request,response){
         var title = 'Web-create';
         var list = templateList(filelist);
         var template = templateHTML(title,list,`
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
     <p><input type="text" name="title" placeholder="title"></p>
     <p><textarea name="description" id="" cols="30" rows="10" placeholder="description"></textarea></p>
     <p><input type="submit"></p>
@@ -103,8 +108,8 @@ var app = http.createServer(function(request,response){
         var title = queryData.id
         var list = templateList(filelist);
         var template = templateHTML(title,list,
-          `<form action="http://localhost:3000/update_process" method="post">
-          <input type="text" name="id" value="${title}">
+          `<form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
           <p><textarea name="description" id="" cols="30" rows="10" placeholder="description">${description}</textarea></p>
           <p><input type="submit"></p>
@@ -114,6 +119,29 @@ var app = http.createServer(function(request,response){
           response.end(template);
           })
         });
+
+    } else if (pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title
+        var description = post.description
+        console.log(post);
+        fs.rename(`data/${id}`, `data/${title}`,function(error){
+          fs.writeFile(`data/${title}`, description,'utf8',
+          function(err){
+            response.writeHead(302,{Location: `/?id=${title}`});
+            response.end();
+          })
+        })
+        /*
+        
+          */
+      });
 
     }else {
         response.writeHead(404);
