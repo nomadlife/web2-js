c3a5885aa510955ec5c49fd68c41cf843b99530f
nomadlife <aaeacd@gmail.com>
2018-08-23 15:16:09 +0900

    nodejs ~ch47 done

--- nodejs/main4.js
+++ nodejs/main4.js
@@ -2,38 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-
-var template = {
-  html:function(title,list,body,control){
-    return `
-    <!doctype html>
-  <html>
-  <head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-  </head>
-  <body>
-  <h1><a href="/">WEB(main4.js)</a></h1>
-  ${list}
-  ${control}
-  ${body}
-  </body>
-  </html>
-    `;
-  },
-  list:function(filelist){
-    var list = '<ul>';
-    var i=0;
-    while(i<filelist.length){
-      list = list+`<li><a href="/?id=${filelist[i]}"> ${filelist[i]} </a></li>`;
-      i=i+1;
-      }
-      list = list + '</ul>';
-    return list;
-  }
-}
-
+var template = require('./lib/template.js')
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -53,18 +24,20 @@ var app = http.createServer(function(request,response){
                 response.end(html);
             })
 
-            
         } else {
           fs.readdir('./data',function(error,filelist){
-            fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+              var filteredId = path.parse(queryData.id).base;
+            fs.readFile(`data/${filteredId}`,'utf8',function(err,description){
             var title = queryData.id
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description);
             var list = template.list(filelist);
             var html = template.html(title,list,
-              `<h2>${title}</h2>${description}`,
+              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             `<a href="/create">create</a>
-            <a href="/update?id=${title}">update</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
             <form action="delete_process" method="post">
-            <input type="hidden" name="id" value="${title}">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
             </form>`);
               response.writeHead(200);
@@ -75,7 +48,7 @@ var app = http.createServer(function(request,response){
         
     } else if (pathname === '/create') {
       fs.readdir('./data',function(error,filelist){
-        console.log(filelist);
+        //console.log(filelist);
         var title = 'Web-create';
         var list = template.list(filelist);
         var html = template.html(title,list,`
@@ -151,7 +124,8 @@ var app = http.createServer(function(request,response){
       request.on('end',function(){
         var post = qs.parse(body);
         var id = post.id;
-        fs.unlink(`data/${id}`,function(error){
+        var filteredId = path.parse(queryData.id).base;
+        fs.unlink(`data/${filteredId}`,function(error){
           response.writeHead(302, {Location:`/`});
           response.end();
         })
