27ae96a1de72e189cbc7d28c1101c87d4624a13f
nomadlife <joonwoo3816@gmail.com>
2018-09-03 11:47:47 +0900

    express ch 14.1 done

--- express/main3.js
+++ express/main3.js
@@ -11,6 +11,7 @@ var compression = require('compression');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(compression());
+
 app.get('*',function(request, response, next){
   fs.readdir('./data', function(error, filelist){
     request.list = filelist;
@@ -25,12 +26,30 @@ app.get('/', function(request, response){
   var html = template.HTML(title, list,
     `<h2>${title}</h2>${description}
     <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px">`,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   response.send(html)
 })
 
-app.get('/page/:pageId', function(request,response){
+
+app.get('/topic/create', function(request, response){
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/topic/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
+})
+
+app.get('/topic/:pageId', function(request,response,next){
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
     if(err){
@@ -44,8 +63,8 @@ app.get('/page/:pageId', function(request,response){
       var list = template.list(request.list);
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
+        ` <a href="/topic/create">create</a>
+          <a href="/topic/update/${sanitizedTitle}">update</a>
           <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
@@ -57,33 +76,18 @@ app.get('/page/:pageId', function(request,response){
   });
 });
 
-app.get('/create', function(request, response){
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-})
 
-app.post('/create_process', function(request,response){
+
+app.post('/topic/create_process', function(request,response){
     var post = request.body;
     var title = post.title;
     var description = post.description;
     fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`page/${title}`);
+      response.redirect(`/topic/${title}`);
     })
 })
 
-app.get('/update/:pageId', function(request,response){
+app.get('/topic/update/:pageId', function(request,response){
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
 
@@ -91,27 +95,27 @@ app.get('/update/:pageId', function(request,response){
     var list = template.list(request.list);
     var html = template.HTML(title, list,
       `
-      <form action="/update_process" method="post">
+      <form action="/topic/update_process" method="post">
         <input type="hidden" name="id" value="${title}">
         <p><input type="text" name="title" placeholder="title" value="${title}"></p>
         <p><textarea name="description" placeholder="description">${description}</textarea></p>
         <p><input type="submit"></p>
       </form>
       `,
-      `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
     );
     response.send(html);
   });
 })
 
-app.post('/update_process',function(request,response){
+app.post('/topic/update_process',function(request,response){
   var post = request.body;
   var id = post.id;
   var title = post.title;
   var description = post.description;
   fs.rename(`data/${id}`, `data/${title}`, function(error){
     fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`page/${title}`);
+      response.redirect(`/topic/${title}`);
     })
   });
 })
@@ -128,6 +132,11 @@ app.post('/delete_process',function(request,response){
 app.use(function(req, res, next){
   res.status(404).send('sorry cant find that!')
 })
+
+app.use(function(err,req,res,nex){
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
