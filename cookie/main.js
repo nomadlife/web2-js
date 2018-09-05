c414646b2f6e1cc3c860007b8e65efe94aed411c
nomadlife <joonwoo3816@gmail.com>
2018-09-05 15:13:44 +0900

    nodejs-cookie finished

--- cookie/main.js
+++ cookie/main.js
@@ -1,4 +1,4 @@
-// for cookie tut
+ // for cookie tut
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -72,6 +72,10 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
+      if(authIsOwner(request, response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = template.list(filelist);
@@ -90,6 +94,10 @@ var app = http.createServer(function(request,response){
         response.end(html);
       });
     } else if(pathname === '/create_process'){
+      if(authIsOwner(request, response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -104,6 +112,10 @@ var app = http.createServer(function(request,response){
           })
       });
     } else if(pathname === '/update'){
+      if(authIsOwner(request, response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -130,6 +142,10 @@ var app = http.createServer(function(request,response){
         });
       });
     } else if(pathname === '/update_process'){
+      if(authIsOwner(request, response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -147,6 +163,10 @@ var app = http.createServer(function(request,response){
           });
       });
     } else if(pathname === '/delete_process'){
+      if(authIsOwner(request, response)===false){
+        response.end('Login required!!');
+        return false;
+      } 
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -197,6 +217,23 @@ var app = http.createServer(function(request,response){
           } else {
             response.end('Who?');
           }
+      });
+    } else if (pathname === '/logout_process') {
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+            response.writeHead(302, {
+              'Set-Cookie':[
+                `email=;Max-Age=0`,
+                `password=;Max-age=0`,
+                `nickname=;Max-age=0`
+              ], 
+              Location: `/`
+            });
+            response.end();
       });
     } else {
       response.writeHead(404);
