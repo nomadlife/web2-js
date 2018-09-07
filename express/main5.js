8e193c8e7c1242150cfa6e74ad5203fe5591c7a3
nomadlife <joonwoo3816@gmail.com>
2018-09-07 14:56:03 +0900

    session ch 6.5 done

---
+++ express/main5.js
@@ -0,0 +1,50 @@
+var express = require('express')
+var app = express()
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var session = require('express-session')
+var FileStore = require('session-file-store')(session)
+
+var helmet = require('helmet');
+app.use(helmet())
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(compression());
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store:new FileStore()
+}))
+
+app.get('*',function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next()
+  });
+})
+
+var indexRouter = require('./routes/index5');
+var topicRouter = require('./routes/topic5')
+var authRouter = require('./routes/auth')
+
+app.use('/',indexRouter);
+app.use('/topic', topicRouter);
+app.use('/auth', authRouter);
+
+
+
+app.use(function(req, res, next){
+  res.status(404).send('sorry cant find that!')
+})
+
+app.use(function(err,req,res,nex){
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+})
