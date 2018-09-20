df46813ed7a6d53f5c310837a36933b97f60eb4e
nomadlife <aaeacd@gmail.com>
2018-09-20 15:31:05 +0900

    Multi-user Passport started, UI/router added

--- express/routes/auth.js
+++ express/routes/auth.js
@@ -37,6 +37,30 @@ module.exports = function (passport) {
       successFlash: true
     }));
 
+    router.get('/register', function (request, response) {
+      auth.testLog(request, response)
+  
+      var fmsg = request.flash();
+      var feedback = '';
+      if (fmsg.error) {
+        feedback = fmsg.error[0];
+      }
+  
+      var title = 'WEB - register';
+      var list = template.list(request.list);
+      var html = template.HTML(title, list, `
+    <div style="color:red;">${feedback}</div>
+    <form action="/auth/register_process" method="post">
+    <p><input type="text" name="email" placeholder="email"></p>
+    <p><input type="password" name="pwd" placeholder="password"></p>
+    <p><input type="password" name="pwd2" placeholder="password"></p>
+    <p><input type="text" name="displayName" placeholder="display name"></p>
+    <p><input type="submit" value="register"></p>
+    </form>
+    `, '');
+      response.send(html);
+    })
+
   router.get('/logout', function (request, response) {
     request.logout();
     response.redirect('/');
